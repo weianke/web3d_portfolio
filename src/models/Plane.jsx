332aa0986147e91a04b231f620278ac7db04644e
anke <245783065@qq.com>
2024-10-08 17:50:05 +0800

    feat: 完成首页3d动效（60%）

---
+++ src/models/Plane.jsx
@@ -0,0 +1,29 @@
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+
+import planeScene from "../assets/3d/plane.glb";
+
+// 3D Model from: https://sketchfab.com/3d-models/stylized-ww1-plane-c4edeb0e410f46e8a4db320879f0a1db
+const Plane = ({ isRotating, ...props }) => {
+  const ref = useRef();
+
+  const { scene, animations } = useGLTF(planeScene);
+
+  const { actions } = useAnimations(animations, ref);
+
+  useEffect(() => {
+    if (isRotating) {
+      actions["Take 001"].play();
+    } else {
+      actions["Take 001"].stop();
+    }
+  }, [actions, isRotating]);
+
+  return (
+    <mesh {...props} ref={ref}>
+      <primitive object={scene} />
+    </mesh>
+  );
+};
+
+export default Plane;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,34 @@
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import Loader from "../components/Loader";
+import Bird from "../models/Bird";
 import Island from "../models/Island";
+import Plane from "../models/Plane";
 import Sky from "../models/Sky";
+
 {
   /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
        
     </div> */
 }
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+
+  const adjustBiplaneForScreenSize = () => {
+    let screenScale, screenPosition;
+
+    // If screen width is less than 768px, adjust the scale and position
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43];
@@ -22,13 +42,18 @@ const Home = () => {
     return [screenScale, screenPosition, rotation];
   };
 
+  const [currentStage, setCurrentStage] = useState(1);
+
+  const [biplaneScale, biplanePosition] = adjustBiplaneForScreenSize();
   const [islandScale, islandPosition, islandRotation] =
     adjustIslandForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
       <Canvas
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
         camera={{
           near: 0.1,
           far: 1000,
@@ -43,11 +68,21 @@ const Home = () => {
             intensity={1}
           />
 
+          <Bird />
           <Sky />
           <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
+            setCurrentStage={setCurrentStage}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+          />
+          <Plane
+            isRotating={isRotating}
+            position={biplanePosition}
+            rotation={[0, 20.1, 0]}
+            scale={biplaneScale}
           />
         </Suspense>
       </Canvas>
